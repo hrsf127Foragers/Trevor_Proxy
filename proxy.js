161c5eef9c77af19d54f66af5e1e4f5c738c57b8
Trevor Puccini <trevorpuccini21@gmail.com>
2020-05-11 22:55:45 -0700

    Build server and enable cross-origin requests within Collections App so it can run on the proxy

---
+++ proxy.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const path = require('path');
+const port = 8000;
+
+app.use(express.static(path.join(__dirname + '/public')));
+
+app.listen(port, () => {console.log(`Server is listening at http://localhost:${port}`)});
+
+// app.get('/', (req, res) => {
+//   res.sendStatus(200);
+// });
